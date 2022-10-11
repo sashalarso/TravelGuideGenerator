6223a31c242d3aa871b7a6b2e4e8cf41c8bcc11b
Jacques Robin <jrobin@fastmail.com>
2022-10-11 11:51:51 +0200

    added supabase auth URL and tokens, configure quasar.config.js and vscode/launch.json

--- src/boot/supabase.js
+++ src/boot/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import useAuthUser from "src/composables/UseAuthUser";
 
-const supabaseUrl = "<YOUR SUPABASE URL GOES HERE>";
-const supabaseKey = "<YOUR SUPABASE KEY GOES HERE>";
+const supabaseUrl = 'https://pxerxbofxcifgdtqccpo.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZXJ4Ym9meGNpZmdkdHFjY3BvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMTQ4MjksImV4cCI6MTk3NTc5MDgyOX0.8y62rUOvH4WnfWmbg-YaLOIEuA0DbOpe3RSiAXunNeo'
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // console.log('init supabase:', supabase)
